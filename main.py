a91327fd8bc02c7710c438546d4d16f45b129448
Igor Martins <iguinhoigor4@gmail.com>
2022-11-25 11:21:33 -0300

    feat: Adicionar função que exibe o menu

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def mostrarMenu():
+    print('==============================================')
+    print('Bem vindo ao Chatbot de atendimento!! :)')
+    print('Os seguintes comandos estão disponíveis:')
+    print('1 - Adicionar item')
+    print('2 - Remover item')
+    print('3 - Mostrar carrinho')
+    print('4 - Fechar carrinho de compras')
+    print('==============================================')
+    print('Sua escolha: ')
+    escolha = input()
+
+    # print(escolha)
+
+
+mostrarMenu()
