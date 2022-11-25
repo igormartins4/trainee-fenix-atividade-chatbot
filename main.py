9a5b58f3979f1b19652f576036dd621def24a539
Igor Martins <iguinhoigor4@gmail.com>
2022-11-25 11:40:18 -0300

    feat: Criar classe do menu

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
+from menu import Menu
 from produto import Produto
 
-def mostrarMenu():
-    print('==============================================')
-    print('Bem vindo ao Chatbot de atendimento!! :)')
-    print('Os seguintes comandos estão disponíveis:')
-    print('1 - Adicionar item')
-    print('2 - Remover item')
-    print('3 - Mostrar carrinho')
-    print('4 - Fechar carrinho de compras')
-    print('==============================================')
-    print('Sua escolha: ')
-    escolha = input()
-
-    # print(escolha)
-
-
-mostrarMenu()
+init = Menu()
---
+++ menu.py
@@ -0,0 +1,16 @@
+class Menu:
+    def __init__(self):
+        # self.escolha = escolha
+        
+        print('==============================================')
+        print('Bem vindo ao Chatbot de atendimento!! :)')
+        print('Os seguintes comandos estão disponíveis:')
+        print('1 - Adicionar item')
+        print('2 - Remover item')
+        print('3 - Mostrar carrinho')
+        print('4 - Fechar carrinho de compras')
+        print('==============================================')
+        print('Sua escolha: ')
+        escolha = input()
+
+        print(escolha)
