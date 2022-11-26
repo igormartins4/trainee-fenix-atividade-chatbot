623f87942d1e4ceea5539b33fe219e1ed38a4256
Igor Martins <iguinhoigor4@gmail.com>
2022-11-26 20:36:48 -0300

    feat: Trocar de classe para função

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from menu import Menu
+from menu import mostrarMenu
 from produto import Produto
 
-init = Menu()
+mostrarMenu()
--- menu.py
+++ menu.py
@@ -1,22 +1,19 @@
-from produto import Produto
 
+def mostrarMenu():
 
-class Menu:
-    def __init__(self):
-        # self.escolha = escolha
+    print('==============================================')
+    print('Bem vindo ao Chatbot de atendimento!! :)')
+    print('Os seguintes comandos estão disponíveis:')
+    print('1 - Adicionar item')
+    print('2 - Remover item')
+    print('3 - Mostrar carrinho')
+    print('4 - Fechar carrinho de compras')
+    print('==============================================')
+    print('Sua escolha: ')
 
-        print('==============================================')
-        print('Bem vindo ao Chatbot de atendimento!! :)')
-        print('Os seguintes comandos estão disponíveis:')
-        print('1 - Adicionar item')
-        print('2 - Remover item')
-        print('3 - Mostrar carrinho')
-        print('4 - Fechar carrinho de compras')
-        print('==============================================')
-        print('Sua escolha: ')
-        escolha = input()
+    escolha = input()
 
-        print(escolha)
+    print(escolha)
 
-        if escolha == 1:
-            Produto.adicionarProduto()
+    if escolha == 1:
+        Produto.adicionarProduto()
